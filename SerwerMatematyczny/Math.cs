60ad94c718ba35423158a0e9a81c8365214fd2c4
Hubert138881 <hubertsiwczynski@gmail.com>
2020-11-08 16:09:33 +0100

    Kopia serwera synchronicznego

---
+++ SerwerMatematyczny/Math.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+/// <summary>
+/// Clasa zawiera metody umożliwiające odczyt i zapis danych z i do serwera
+/// </summary>
+public class Server_Operations {
+    protected NetworkStream stream;
+    protected BinaryWriter writer;
+    private byte[] clear = new byte[1024];
+    /// <summary>
+    /// Nadanie wartości writerowi - obiektowi służącemu do łatwiejszej obsługi wyjścia
+    /// </summary>
+    /// <param name="stream">Strumień danych typu NetworkStream</param>
+    public Server_Operations(NetworkStream stream) {
+        this.stream = stream;
+             writer = new BinaryWriter(stream);
+    }
+    /// <summary>
+    /// funkcja zapisu wraz z impementacją dla różnego typu zmienych
+    /// </summary>
+    /// <param name="msg">Tekst do wysłania</param>
+    public void Send_message(string msg) {
+        writer.Write(msg);
+    }
+    public void Send_message(int msg)
+    {
+        writer.Write(msg);
+    }
+    public void Send_message(float msg)
+    {
+        writer.Write(msg);
+    }
+    /// <summary>
+    /// Funkcja służąca do pobierania danych wpisanych do użytkownika
+    /// </summary>
+    /// <returns>Zwraca numer</returns>
+    public int Get_int()
+    {
+        byte[] buffer = new byte[1024];
+        int msg_len = stream.Read(buffer, 0, 1024);
+        string result = "";
+        /*
+         * Czytany jest tekst właściwy do bufora, a następnie czyszczony niepotrzebny enter. 
+         * Pobrane dane są zamieniane na string, a później dla wygody na int
+         */
+        stream.Read(clear, 0, 1024);
+        foreach(byte b in buffer)
+            result += Convert.ToChar(b);
+        return Convert.ToInt32(result);
+    }
+}
+public class Math
+{
+	public Math() {
+	}
+    /// <summary>
+    /// Zwraca silnię z liczny n
+    /// </summary>
+    /// <param name="n">Liczba z której liczyby silnię</param>
+    /// <returns></returns>
+    static public int Silnia(int n) {
+        if (n == 0) return 1;
+        return n * Silnia(n - 1);
+    }
+}
---
+++ SerwerMatematyczny/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwerMatematyczny
+{
+    class Program {
+        static void Main(string[] args) {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 2048);
+            server.Start();
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+
+            Server_Operations o = new Server_Operations(stream);
+
+            while (true) {
+                o.Send_message("Podaj opcję: \n\r1. Dodawanie \n\r2. Odejmowanie \n\r3. Mnożenie \n\r4. Dzielenie \n\r5. Silnia\n\r");
+                int response = o.Get_int();
+                o.Send_message("Podaj liczbę: ");
+                int a = o.Get_int(), b = 0
+                float wynik = 0;
+                switch (response) {
+                    case 1:
+                        o.Send_message("\n\rPodaj drugą liczbę: ");
+                        b = o.Get_int();
+                        wynik = a + b;
+                        break;
+                    case 2:
+                        o.Send_message("\n\rPodaj drugą liczbę: ");
+                        b = o.Get_int();
+                        wynik = a - b;
+                        break;
+                    case 3:
+                        o.Send_message("\n\rPodaj drugą liczbę: ");
+                        b = o.Get_int();
+                        wynik = a * b;
+                        break;
+                    case 4:
+                        o.Send_message("\n\rPodaj drugą liczbę: ");
+                        b = o.Get_int();
+                        wynik = a / b;
+                        break;
+                    case 5:
+                        wynik = Math.Silnia(a);
+                        break;
+                }
+                o.Send_message("Wynik: " + wynik + "\n\r");
+            }
+        }
+    }
+}
